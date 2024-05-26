ed25863bf8e8ccdbde2b4f91aab36bc0be71a2ba
Cobb Lee <49636918+archepro84@users.noreply.github.com>
2024-05-26 15:13:53 +0900

    style: ReFormat by prettier

--- src/common/elysia.ts
+++ src/common/elysia.ts
@@ -1,6 +1,5 @@
-import { Elysia } from 'elysia';
+import { Elysia } from 'elysia'
 
-
-export const createElysia =
-  (config?: ConstructorParameters<typeof Elysia>[0]) =>
-    new Elysia({ ...config });
+export const createElysia = (
+  config?: ConstructorParameters<typeof Elysia>[0]
+) => new Elysia({ ...config })
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,16 @@
-import { createElysia } from "./common/elysia";
+import { createElysia } from './common/elysia'
 
-const PORT = 3000;
+const PORT = 3000
 
 const elysia = createElysia()
-  .get('', "Elysia is running!")
+  .get('', 'Elysia is running!')
   .get('ping', async () => {
     return {
       success: true,
-      data: "pong"
+      data: 'pong',
     }
-  });
-
+  })
 
 elysia.listen({ port: PORT }, ({ hostname, port }) => {
-  console.log(`Elysia is running on http://${hostname}:${port}`);
+  console.log(`Elysia is running on http://${hostname}:${port}`)
 })
